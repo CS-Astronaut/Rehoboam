b121a56a87d241bae24a5963580c2843847db167
amin farahani <aminfvrvhvni@gmail.com>
2026-08-02 03:25:45 +0330

    fix(common): add SCRIPT_DIR to PYTHONPATH in common.sh

--- common.sh
+++ common.sh
@@ -6,6 +6,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PYTHON_EXEC="python3"
 DB_CLI="$SCRIPT_DIR/rehoboam_db.py"
+export PYTHONPATH="$SCRIPT_DIR${PYTHONPATH:+:$PYTHONPATH}"
 
 # ---------------------------------------------------------------------------
 # Config
